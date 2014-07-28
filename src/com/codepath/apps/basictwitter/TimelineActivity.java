ada3f6ecc200d5804ee5c19bda62e317fd928548
Julio Nobrega Netto <inerte@gmail.com>
2014-07-27 17:52:33 -0700

    Removed icons from Home/Mentions action bar tabs

--- src/com/codepath/apps/basictwitter/TimelineActivity.java
+++ src/com/codepath/apps/basictwitter/TimelineActivity.java
@@ -4,45 +4,45 @@ import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.widget.Toast;
 
 import com.codepath.apps.basictwitter.fragments.HomeTimelineFragment;
 import com.codepath.apps.basictwitter.fragments.MentionsTimelineFragment;
 import com.codepath.apps.basictwitter.listeners.FragmentTabListener;
 
 public class TimelineActivity extends FragmentActivity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_timeline); 
-		setupTabs();
-	}
-	
-	private void setupTabs() {
-		ActionBar actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		actionBar.setDisplayShowTitleEnabled(true);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        setupTabs();
+    }
 
-		Tab tab1 = actionBar
-			.newTab()
-			.setText("Home")
-			.setIcon(R.drawable.ic_home)
-			.setTag("HomeTimelineFragment")
-			.setTabListener(
-				new FragmentTabListener<HomeTimelineFragment>(R.id.flContainer, this, "home", HomeTimelineFragment.class));
+    private void setupTabs() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(true);
 
-		actionBar.addTab(tab1);
-		actionBar.selectTab(tab1);
+        Tab tab1 = actionBar
+                .newTab()
+                .setText("Home")
+                .setTag("HomeTimelineFragment")
+                .setTabListener(
+                        new FragmentTabListener<HomeTimelineFragment>(
+                                R.id.flContainer, this, "home",
+                                HomeTimelineFragment.class));
 
-		Tab tab2 = actionBar
-			.newTab()
-			.setText("Mentions")
-			.setIcon(R.drawable.ic_mentions)
-			.setTag("MentionsTimelineFragment")
-			.setTabListener(
-			    new FragmentTabListener<MentionsTimelineFragment>(R.id.flContainer, this, "mentions",
-			    		MentionsTimelineFragment.class));
+        actionBar.addTab(tab1);
+        actionBar.selectTab(tab1);
 
-		actionBar.addTab(tab2);
-	}
+        Tab tab2 = actionBar
+                .newTab()
+                .setText("Mentions")
+                .setTag("MentionsTimelineFragment")
+                .setTabListener(
+                        new FragmentTabListener<MentionsTimelineFragment>(
+                                R.id.flContainer, this, "mentions",
+                                MentionsTimelineFragment.class));
+
+        actionBar.addTab(tab2);
+    }
 }
